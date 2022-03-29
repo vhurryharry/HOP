ab475883b67dc138463fa2c5ef1d6718bdc5aa35
Alexander Bai <vhurryharry@gmail.com>
2022-03-29 00:24:30 -0400

    stock api handler

--- app.py
+++ app.py
@@ -3,14 +3,8 @@ from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS  # comment this on deployment
 from api.StockApiHandler import StockApiHandler
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__)
 CORS(app)  # comment this on deployment
 api = Api(app)
 
-
-@app.route("/", defaults={'path': ''})
-def serve(path):
-    return send_from_directory(app.static_folder, 'index.html')
-
-
-api.add_resource(StockApiHandler, '/flask/api')
+api.add_resource(StockApiHandler, '/api/<string:symbol>')
